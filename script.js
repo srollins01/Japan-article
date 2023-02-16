ee7d7e871bab460c853ddfcd39cf1cde67bbfed3
Sarah Rollins <s.rollins751@gmail.com>
2023-02-16 21:40:40 +0900

    Japan article page

---
+++ script.js
@@ -0,0 +1,60 @@
+const modal = document.getElementById("myModal");
+
+
+const lnk = document.getElementById("myLnk");
+
+
+const span = document.getElementsByClassName("close")[0];
+
+
+lnk.addEventListener("click", (event) => {
+  modal.style.display = "block";
+});
+
+
+span.addEventListener("click", (event) => {
+  modal.style.display = "none";
+});
+
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+
+function openModal() {
+  document.getElementById("myModallr").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModallr").style.display = "none";
+}
+
+let slideIndex = 1;
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("demo");
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  for (i = 0; i < slides.length; i += 1) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i += 1) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
